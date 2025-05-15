22a89735e33187cc4c8ea4f3bed1f3a69e575cb8
B.P. Power Engineering Group Co.,Ltd <sales@bpthai.com>
2025-05-15 16:18:40 +0700

    Add Java
    Nice

---
+++ Chatbot.js
@@ -0,0 +1,4 @@
+document.getElementById("chat-icon").addEventListener("click", function() {
+    con chatBox = document.getElementById("chat-box");
+    chatBox.style.display = chatBox.style.display === "none" ? "block" : "none";
+});
