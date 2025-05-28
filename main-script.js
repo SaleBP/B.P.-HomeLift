e65ec02df09d18f277f33a620aa0ba4e817a18fa
B.P. Power Engineering Group Co.,Ltd <sales@bpthai.com>
2025-05-28 14:56:10 +0700

    ร

--- main-script.js
+++ main-script.js
@@ -31,16 +31,6 @@ window.onload = function () {
   }, 16);
 };
 
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    const toggle = document.getElementById("quickbar-toggle");
-    const menu = document.getElementById("quickbar-menu");
-
-    toggle.addEventListener("click", function() {
-      menu.classList.toggle("show");
-    });
-  });
-</script>
 
   document.addEventListener("DOMContentLoaded", () => {
     const wrapper = document.querySelector(".nav-contact-wrapper");
