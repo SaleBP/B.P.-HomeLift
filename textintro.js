0244d65e989afce2d631e7d1638e854aea50df33
B.P. Power Engineering Group Co.,Ltd <sales@bpthai.com>
2025-06-13 14:52:47 +0700

    แถแช

--- textintro.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const letters = document.querySelectorAll(".textintro span");
-
-  letters.forEach((span, index) => {
-    span.style.animationDelay = `${index * 0.1}s`;
-  });
-});
