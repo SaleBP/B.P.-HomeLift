660c8765742e090951b205044cd789039d4c7351
B.P. Power Engineering Group Co.,Ltd <sales@bpthai.com>
2025-06-20 15:51:56 +0700

    A

--- main-script.js
+++ main-script.js
@@ -186,3 +186,28 @@ document.addEventListener("DOMContentLoaded", () => {
   fadeUps.forEach(el => observer.observe(el));
 });
 
+  window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      document.querySelector('.top-bar').classList.add('show');
+    }, 4500); // 4 วินาที = 4000 มิลลิวินาที
+  });
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("quickbar-toggle");
+    const panel = document.getElementById("quickbar-panel");
+    const closeBtn = document.getElementById("quickbar-close");
+
+    toggle.addEventListener("click", () => {
+      panel.classList.add("open");
+    });
+
+    closeBtn.addEventListener("click", () => {
+      panel.classList.remove("open");
+    });
+
+    document.addEventListener("click", (e) => {
+      if (panel.classList.contains("open") && !panel.contains(e.target) && e.target !== toggle) {
+        panel.classList.remove("open");
+      }
+    });
+  });
