063ffb235e29750cde60b427072ef21a40fa9680
B.P. Power Engineering Group Co.,Ltd <sales@bpthai.com>
2025-08-21 14:40:13 +0700

    d

--- Tyinstall.js
+++ Tyinstall.js
@@ -31,3 +31,56 @@ document.addEventListener("DOMContentLoaded", () => {
   updateFocusedImage();
 });
 
+
+// Typeinstall.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollContainer = document.querySelector(".scroll-container");
+
+  if (scrollContainer) {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    // ป้องกันการ drag image ออกไป
+    const images = scrollContainer.querySelectorAll("img");
+    images.forEach(img => {
+      img.addEventListener("dragstart", (e) => e.preventDefault());
+    });
+
+        function momentumScroll() {
+      scrollContainer.scrollLeft -= velocity;
+      velocity *= 0.99; // ค่าหน่วง (0.9 = ลื่น, 0.99 = ลื่นมาก, 0.8 = หยุดไว)
+
+      if (Math.abs(velocity) > 0.5) {
+        momentumID = requestAnimationFrame(momentumScroll);
+      }
+    }
+
+    scrollContainer.addEventListener("mousedown", (e) => {
+      isDown = true;
+      scrollContainer.classList.add("active");
+      startX = e.pageX - scrollContainer.offsetLeft;
+      scrollLeft = scrollContainer.scrollLeft;
+    });
+
+    scrollContainer.addEventListener("mouseleave", () => {
+      isDown = false;
+      scrollContainer.classList.remove("active");
+    });
+
+    scrollContainer.addEventListener("mouseup", () => {
+      isDown = false;
+      scrollContainer.classList.remove("active");
+    });
+
+    scrollContainer.addEventListener("mousemove", (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - scrollContainer.offsetLeft;
+      const walk = (x - startX) * 1.5; // ปรับความไว
+      scrollContainer.scrollLeft = scrollLeft - walk;
+    });
+  }
+});
+
